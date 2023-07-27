c424d22640704e5f633956c26b6566be49a03620
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-07-27 09:27:02 +0200

    design navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,26 +5,41 @@ import { Link } from "react-router-dom";
 function NavBar() {
   const {isLoggedIn, logOutUser, user} = useContext(AuthContext);
 
-  return(
-    <div className="div-navbar">
-      <nav>        
-        <Link to="/feeds">Feeds</Link>
-        {!isLoggedIn && (
-        <>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Login</Link>
-        </>
-      )}
+  const toggleHamburgerMenu = () => {
+    const navbarElement = document.getElementById("navbar");
+          navbarElement.classList.toggle("active");
+    
+    var hamburgerMenu = document.getElementById("hamburger-menu");
+        hamburgerMenu.classList.toggle("active");
+  }
 
-      {isLoggedIn && (
-        <>
-          <Link to="/myhome">My Home</Link>
-          <Link to={`/profile/${user._id}`}>Profile</Link>
-          <Link onClick={logOutUser}>Log Out</Link>
-        </>
-      )}
-      </nav>
-    </div>
+
+  return(
+      <header>
+        <input type="checkbox" id="input-hamburger-menu"/>
+        <div id="hamburger-menu" onClick={toggleHamburgerMenu}>
+          <span className="top-span span-hamburger"></span>
+          <span className="middle-span span-hamburger"></span>
+          <span className="bottom-span span-hamburger"></span>
+        </div>
+        <nav id="navbar">
+          <Link to="/feeds">Feeds</Link>
+          {!isLoggedIn && (
+          <>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
+
+        {isLoggedIn && (
+          <>
+            <Link to="/myhome">My Home</Link>
+            <Link to={`/profile/${user._id}`}>Profile</Link>
+            <Link onClick={logOutUser}>Log Out</Link>
+          </>
+        )}
+        </nav>
+      </header>
   )
 };
 
