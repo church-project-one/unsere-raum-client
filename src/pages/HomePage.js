0f0db859230748190202eef08475e0844104bc4d
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-08-28 19:52:15 +0200

    styling home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,12 @@ import { Link } from "react-router-dom";
 function HomePage() {
   return(
     <div id="home-page">
-      <h2>Feel free to make your own to do list</h2>
+      <p>This is a website application to help people make their own room,<br></br>
+          and inside the room they can
+          add activities according to its needs.
+      <br></br>
+      <br></br>
+      </p>
       <p>Please, sign up if you don't have an account or login if you do!</p>
       
       <Link to="/signup">Sign Up!</Link> <p> or </p>
